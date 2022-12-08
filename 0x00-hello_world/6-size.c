e99c30b4ff2d609060a0a3325c571a206c6633d3
Ismael105 <esmaelmohammed105.com>
2022-12-08 05:12:49 -0800

    hh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /**
-*main -Entry point
-*
+*main - Entry point
 *Description: 'This program tells us the size of char, int, long int, long long int, and float'
-*
 *Return - Always 0 (Success)
 */
 int main(void)
