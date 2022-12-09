152d466d8c418ed518d657627b51dde45d42aca2
Ismael105 <esmaelmohammed105.com>
2022-12-09 10:30:20 -0800

    kk

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int s, d;
+for (s = 0; s < 10; s++)
+putchar("%d", s);
+putchar('\n');
+return (0);
+}
