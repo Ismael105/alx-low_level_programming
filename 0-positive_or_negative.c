d38b621f23148ce64e67fde7e610ca86c51ed129
Ismael105 <esmaelmohammed105.com>
2022-12-09 00:06:25 -0800

    Positive anything is better than negative nothing

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -19,7 +19,18 @@ int main(void)
   srand(time(0));
   
   n = rand() - RAND_MAX / 2;
-  
+  if (n<0)
+    {
+      printf("is negative");
+    }
+  else if (n==0)
+    {
+      printf("is zero");
+    }
+  else
+    {
+      printf"is positive");
+    }
   /* your code goes there */
   
   return (0);
