72deabc73d1f252d2618bd5635e1d5e253a853f1
Ismael105 <esmaelmohammed105.com>
2022-12-09 05:24:56 -0800

    kk

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,11 +6,11 @@
 int main(void)
 {
 char i;
-for (i == 'a'; i <= 'z'; i++)
+for (i == 'a'; i <= 'z'; i++){
 if (i == 'e' || i == 'q')
 continue;
 else
-putchar(i);
+putchar(i);}
 putchar('\n');
 return (0);
 }
