7af919320e43a0c19f461cfb72675cdd273b7687
Ismael105 <esmaelmohammed105.com>
2022-12-09 04:36:26 -0800

    kk

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *Retur: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
 char i;
-for (i='a'; i<='z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 putchar(i);
 putchar('\n');
 return (0);
