33d9976cdaa5867579aede2108cbd14f280d73b2
Ismael105 <esmaelmohammed105.com>
2022-12-10 09:48:24 -0800

    kk

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a, b, c;
+for (a = 0; a < 10; a++)
+{
+for (b = 1; b < 10; b++)
+{
+for (c = 1; c < 10; c++)
+{
+if (a != b && a != c && b != c)
+{
+if (a < b && b < c)
+{
+putchar(a + '0');
+  
+		      putchar(b + '0');
+		      
+		      putchar(c + '0');
+		      
+		      if (a != 7)
+			
+			{
+			  
+			  putchar(',');
+			  
+			  putchar(' ');
+			  
+			}}}}}}
+  
+  putchar('\n');
+  
+  return (0);
+  
+}
