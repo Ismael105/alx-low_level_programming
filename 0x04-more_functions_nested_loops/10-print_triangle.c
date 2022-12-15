2fe57e8da5da8be2447a768f30336d452523ecd6
Ismael105 <esmaelmohammed105.com>
2022-12-15 12:39:44 -0800

    aa

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,20 +6,25 @@
  */
 void print_triangle(int size)
 {
-int i, j;
-if (size <= 0)
+int i, h;
+if (size > 0)
 {
+for (h = 1; i <= size; h++)
+{
+for (i = size - h; i > 0; i--)
+{
+_putchar(' ');
+}
+for (i = 0; i < h; i++)
+{
+_putchar('#');
+}
+if (h == size)
+{
+continue;
+}
 _putchar('\n');
 }
-else
-{
-for (i = 1; i <= size; ++i)
-{
-for (j = 1; j <= i; ++j)
-{
-_putchar("# ");
 }
 _putchar("\n");
-}
-return (0);
 }
