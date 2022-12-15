23de0fcb4ead991ea853cfc5d64bfa98215025e2
Ismael105 <esmaelmohammed105.com>
2022-12-15 14:16:50 -0800

    dd

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,21 @@
 #include "main.h"
 /**
  * print_most_numbers- print numbers except 2 and 4
+ *Return: void
  */
 void print_most_numbers(void)
 {
-char i;
-for (i = '0'; i <= '9'; i++)
+int i;
+for (i = 48; i <= 58; i++)
 {
-if (i != '2' || i != '4')
+if (i != 50 || i != 52)
 {
 _putchar(i);
 }
+else
+{
+continue;
+}
 }
 _puthchar('\n');
-return (0);
 }
