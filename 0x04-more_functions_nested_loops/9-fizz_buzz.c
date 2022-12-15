24b983f19c7c5a1be2c9933d15fd8dcffc1b4d4b
Ismael105 <esmaelmohammed105.com>
2022-12-15 09:44:00 -0800

    ss

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print numbers 0 to 100, not multiple of 3 and 5
+ * Return: void
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 100)
+{
+if (i % 3 == 0 && i % 5 == 0)
+putchar("FizzBuzz");
+else if (i % 3 == 0)
+putchar("Fizz");
+else
+{
+printf(i);
+}
+pritf(" ");
+}
+}
