28e0532ce2b0ace0e662c7b068a2a6d92689f6ae
Ismael105 <esmaelmohammed105.com>
2022-12-15 04:22:49 -0800

    dd

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio>
+/**
+ *print_numbers - prints numbers form zero to nine
+ *Return: 0 Always (Success)
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i + '0');
+_putchar("\n");
+}
+return (0);
+}
