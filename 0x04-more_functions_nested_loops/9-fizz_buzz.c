d31c773bfd7de76e812d6514dea60b5ae21db24d
Ismael105 <esmaelmohammed105.com>
2022-12-15 10:31:11 -0800

    ss

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 /**
  * main - print numbers 0 to 100, not multiple of 3 and 5
@@ -8,23 +6,29 @@
 int main(void)
 {
 int i;
+char str1[5];
+char str2[5];
+char str3[9];
+strcpy(str1, "Fizz");
+strcpy(str2, "Buzz");
+strcpy(str3, "FizzBuzz");
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
-printf("%s.", FizzBuzz);
+puts(str3);
 }
 else if (i % 3 == 0)
 {
-printf("%s.", Fizz);
+puts(str1);
 }
 else if (i % 5 == 0)
 {
-printf("%s.", Buzz);
+puts(str2);
 }
 else
 {
-printf("%d", i);
+printf("%d ", i);
 }
 printf(" ");
 }
