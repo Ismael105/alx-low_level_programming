16691a2522f6d001740aaa25078c6ecb49d09d58
Ismael105 <esmaelmohammed105.com>
2022-12-15 05:07:04 -0800

    ss

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  *print_numbers - prints numbers 0123456789\n
   */
@@ -8,7 +7,7 @@ void print_numbers(void)
 char i;
 for (i = '0'; i <= '9'; i++)
 {
-_putchar(c);
+_putchar(i);
 }
 _puthchar('\n');
 }
