a31f5beda7949c2c16688132a8cca73e8b3e7fb2
Ismael105 <esmaelmohammed105.com>
2022-12-16 08:49:07 -0800

    aa

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,8 @@ void print_most_numbers(void)
 char i;
 for (i = '0'; i <= '9'; i++)
 {
-if (i != '2' || i != '4')
-{
+if (!(i == '2' || i == '4'))
 _putchar(i);
-}
-else
-{
-continue;
-}
 }
 _puthchar('\n');
 }
