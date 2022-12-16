4fa99cbaa2c10229d81d978b58288c05a38f582d
Ismael105 <esmaelmohammed105.com>
2022-12-16 08:37:40 -0800

    ss

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,15 +7,12 @@
 void print_diagonal(int n)
 {
 int i;
-if (n <= '0')
-{
-_putchar('\n');
-}
-else
+if (n > 0)
 {
 for (i = 0; i < n; i++)
 {
-_putchar("\ \n");
+_putchar('\ ');
 }
 }
+_putchar('\n');
 }
