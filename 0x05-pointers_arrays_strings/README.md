c810b898090b032bf7a5f9cbb154455aa2ca3147
Ismael105 <esmaelmohammed105@gmail.com>
2022-12-19 10:03:59 -0800

    ss

--- 0x05-pointers_arrays_strings/README.md
+++
@@ -1,1 +0,0 @@
-this is a socond readme file
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef main_h
+#define main_h
 void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
 
 #endif
