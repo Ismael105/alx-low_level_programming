07767f3fc25ee9371b97935c4a79a6e3cf78e2c6
Ismael105 <esmaelmohammed105@gmail.com>
2022-12-20 02:19:59 -0800

    aa

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,43 +5,37 @@
 #include <unistd.h>
 int main() 
 {
-  int length;
-  scanf("%d", length);
-  if (length <= 0)
-    {
-      printf("password length must greater than 0");
-      return (1);
-    }
+int length;
+scanf("%d", length);
+if (length <= 0)
+{
+printf("password length must greater than 0");
+return (1);
+}
 char *password = malloc(length + 1);
 char digits[] = "0123456789";
- int digits_length = strlen(digits);
- char letter[] = "abcdefghijklmnoqprstuvwyzx";
- int letter_length = strlen(letter);
+int digits_length = strlen(digits);
+char letter[] = "abcdefghijklmnoqprstuvwyzx";
+int letter_length = strlen(letter);
 char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
- int LETTER_length = strlen(LETTER);
+int LETTER_length = strlen(LETTER);
 char symbols[] = "!@#$^&*?";
- int symbols_length = strlen(symbols);
- srand(time(NULL) * getpid());
- for (int i = 0; i < length; i++) {
-   int char_type = rand() % 4;
-   if (char_type == 0) {
+int symbols_length = strlen(symbols);
+srand(time(NULL) * getpid());
+for (int i = 0; i < length; i++) {
+int char_type = rand() % 4;
+if (char_type == 0) {
 password[i] = digits[rand() % digits_length];
-   }
-else if (char_type == 1) {
-password[i] = symbols[rand() % 8];
-randomizer = rand() % 4;
-printf("%c", password[i]);
 }
-else if (randomizer == 3) {
-password[i] = LETTER[rand() % 26];
-randomizer = rand() % 4;
-printf("%c", password[i]);
+else if (char_type == 1) {
+password[i] = letter[rand() % letter_length];
+}
+else if (char_type == 2) {
+password[i] = LETTER[rand() % LETTER];
 }
 else {
-password[i] = letter[rand() % 26];
-randomizer = rand() % 4;
-printf("%c", password[i]);
+password[i] = symbols[rand() % symbols_length];
 }
-}
+free(password);
 return 0; 
 }
