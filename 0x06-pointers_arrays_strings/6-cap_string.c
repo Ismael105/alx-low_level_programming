f889ef837fe2207e6ec91d5043d0f6e6c6abaece
Ismael105 <esmaelmohammed105@gmail.com>
2022-12-21 23:24:35 -0800

    aa

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * cap_string - function to capitalize the first
- * character of a string
+ * cap_string - function to capitalize first
  * @str: string
- * Return: capital string
+ * Return: capital
  */
 char *cap_string(char *str)
 {
@@ -11,9 +10,19 @@ int i = 0;
 while (!(str[++i] >= 'a' && str[i] <= 'z')
 {
 i++;
- if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || i == 0;)
+if (str[i - 1] == ' ' ||
+str[i - 1] == '\t' ||
+str[i - 1] == '\n' ||
+str[i - 1] == ',' ||
+str[i - 1] == '.' ||
+str[i - 1] == '!' ||
+str[i - 1] == '?' ||
+str[i - 1] == '"' ||
+str[i - 1] == '(' ||
+str[i - 1] == ')' ||
+str[i - 1] == '{' ||
+str[i - 1] == '}')
 str[i - 1] -= 32;
-i++;
 }
 return (str);
 }
