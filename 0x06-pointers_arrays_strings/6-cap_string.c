4f088bb05ee90d023e66667aef5bab06f8530eb1
Ismael105 <esmaelmohammed105@gmail.com>
2022-12-22 03:02:24 -0800

    aa

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,21 +9,20 @@ char *cap_string(char *str)
 int i = 0;
 while (!(str[++i] >= 'a' && str[i] <= 'z'))
 {
-i++;
-if (str[i - 1] == ' ' ||
-str[i - 1] == '\t' ||
-str[i - 1] == '\n' ||
-str[i - 1] == ',' ||
-str[i - 1] == '.' ||
-str[i - 1] == '!' ||
-str[i - 1] == '?' ||
-str[i - 1] == '"' ||
-str[i - 1] == '(' ||
-str[i - 1] == ')' ||
-str[i - 1] == '{' ||
-str[i - 1] == '}' ||
+if (str[i] == ' ' ||
+str[i] == '\t' ||
+str[i] == '\n' ||
+str[i] == ',' ||
+str[i] == '.' ||
+str[i] == '!' ||
+str[i] == '?' ||
+str[i] == '"' ||
+str[i] == '(' ||
+str[i] == ')' ||
+str[i] == '{' ||
+str[i] == '}' ||
 i == 0)
-str[i - 1] -= 32;
+str[i] -= 32;
 i++;
 }
 return (str);
