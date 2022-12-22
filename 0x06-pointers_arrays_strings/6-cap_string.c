97a77cdf45b66391456642fae7fe6f965f96902f
Ismael105 <esmaelmohammed105@gmail.com>
2022-12-21 23:12:03 -0800

    ss

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,6 +13,7 @@ while (!(str[++i] >= 'a' && str[i] <= 'z')
 i++;
  if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || i == 0;)
 str[i - 1] -= 32;
+i++;
 }
 return (str);
 }
