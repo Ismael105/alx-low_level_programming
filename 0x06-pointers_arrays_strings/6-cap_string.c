e88dc4af04989015d88d6485087628c9ca40e17a
Ismael105 <esmaelmohammed105@gmail.com>
2022-12-22 03:00:17 -0800

    aa

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,22 +7,23 @@
 char *cap_string(char *str)
 {
 int i = 0;
-while (!(str[i] >= 'a' && str[i] <= 'z'))
+while (!(str[++i] >= 'a' && str[i] <= 'z'))
 {
-if (str[i] == ' ' ||
-str[i] == '\t' ||
-str[i] == '\n' ||
-str[i] == ',' ||
-str[i] == '.' ||
-str[i] == '!' ||
-str[i] == '?' ||
-str[i] == '"' ||
-str[i] == '(' ||
-str[i] == ')' ||
-str[i] == '{' ||
-str[i] == '}' ||
+i++;
+if (str[i - 1] == ' ' ||
+str[i - 1] == '\t' ||
+str[i - 1] == '\n' ||
+str[i - 1] == ',' ||
+str[i - 1] == '.' ||
+str[i - 1] == '!' ||
+str[i - 1] == '?' ||
+str[i - 1] == '"' ||
+str[i - 1] == '(' ||
+str[i - 1] == ')' ||
+str[i - 1] == '{' ||
+str[i - 1] == '}' ||
 i == 0)
-str[i] -= 32;
+str[i - 1] -= 32;
 i++;
 }
 return (str);
