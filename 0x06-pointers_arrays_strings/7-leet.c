21d4928013c71abf7e8d28c6a5891e00a5d5eba8
Ismael105 <esmaelmohammed105@gmail.com>
2022-12-22 00:11:10 -0800

    aa

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * leet - function to encode a string
- * @str: string
- * Return: encoded string
+ * *leet - function to encode a string
+ * @str: string value
+ * Return: return encoded string
  */
-char *leet(char *)
+char *leet(char *str)
 {
 int i = 0;
 int j;
@@ -13,7 +13,8 @@ while (str[++i])
 {
 for (j = 0; j <= 7; j++)
 {
-if (str[i] == leet[j] || str[i] - 32 == leet[j])
+if (str[i] == leet[j] ||
+str[i] - 32 == leet[j])
 str[i] = j + '0';
 }
 }
