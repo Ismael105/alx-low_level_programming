108f47940fcbdeb18cd14da49d21034ac22acc82
Ismael105 <esmaelmohammed105@gmail.com>
2022-12-24 11:55:09 -0800

    gg

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,16 +10,18 @@ for (a = 0; a < 10; a++)
 {
 for (b = 1; b < 10; b++)
 {
-for (c = 1; c < 10; c++)
+for (c = 2; c < 10; c++)
 {
-if (a != b && a != c && b != c)
+if (a < b && b < c)
 {
 putchar(a + '0');
 putchar(b + '0');
-putchar(c + '0');      
+putchar(c + '0');
+if (a != 7)
+{
 putchar(44);
 putchar(32);
-}}}}
+}}}}}
 putchar('\n');
 return (0);
 }
