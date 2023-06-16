1d45bcfd063023fd16afd036e97c27b67b82e82e
Ismael105 <esmaelmohammed105@gmail.com>
2023-06-16 04:40:24 -0700

    dd

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,35 +5,21 @@
 */
 int main(void)
 {
-int a, b, c;
-for (a = 0; a < 10; a++)
+  int p, q;
+  for (p = 0; p <= 98; p++)
 {
-for (b = 1; b < 10; b++)
+  for (q = p + 1; q < 99; q++)
 {
-for (c = 1; c < 10; c++)
-{
-if (a != b && a != c && b != c)
-{
-if (a < b && b < c)
-{
-putchar(a + '0');
-  
-		      putchar(b + '0');
-		      
-		      putchar(c + '0');
-		      
-		      if (a != 7)
-			
-			{
-			  
-			  putchar(',');
-			  
-			  putchar(' ');
-			  
-			}}}}}}
-  
-  putchar('\n');
-  
-  return (0);
-  
+  putchar((p / 10) + '0');
+  putchar((p % 10) + '0');
+putchar(' ');
+ putchar((q / 10) + '0');
+ putchar((q % 10) + '0');
+ if (p == 98 && q == 99)
+   continue;
+putchar(',');
+putchar(' ');
+}}
+putchar('\n');
+return (0);
 }
