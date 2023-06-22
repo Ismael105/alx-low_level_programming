941d1f110e43e97f7ad0f9fc3944f7a03a499256
Ismael105 <esmaelmohammed105@gmail.com>
2023-06-22 03:30:56 -0700

    aa

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _isalpha-identifies letters from other characters
- * int c-it is any kind of input
- * return-returns 1 and 0 based on the c value 
- */
+ * int c-is any kind of input
+ * return() - returns 1 and 0 based on the c value
+*/
 int _isalpha(int c)
 {
 if (c >= 'a' && c <= 'z')
